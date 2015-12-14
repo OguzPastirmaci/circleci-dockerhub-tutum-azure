fde7f3a027c38b6831aa5cbdb1a805b200c37ac1
Oguz Pastirmaci <oguzp@microsoft.com>
2015-12-14 15:58:48 -0800

    Added jade

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ var express = require('express');
 
 var PORT = 8000;
 
-var app = express();  
-app.get('/', function (req, res) {  
-  res.send('Hello from Docker app!!\n');
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+      res.render('index', {
+	});
 });
 
 if(!module.parent){
 app.listen(PORT);
 }
+
 console.log('Running on port ' + PORT);
 module.exports.getApp = app;
